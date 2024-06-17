931599cb44c6c15151ffb194cc67aca9effd95db
MechSoft <123365939+MechSoft371@users.noreply.github.com>
2024-06-17 17:47:11 +0530

    new changes

---
+++ ss1.js
@@ -0,0 +1,20 @@
+
+
+var firstname = "prudhvi"
+
+var lastname = "raj"
+
+var fullname = firstname+ " "+lastname
+console.log(fullname)
+
+var upname = fullname.toUpperCase()
+console.log(upname)
+
+var lpname = fullname.toLowerCase()
+console.log(lpname)
+
+var spname = fullname+" @"
+console.log(spname)
+
+var trname = spname.trimEnd(" ")
+console.log(trname)
